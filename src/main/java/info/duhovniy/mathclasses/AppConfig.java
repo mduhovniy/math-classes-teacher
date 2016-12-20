c3a20b71cb6d282b91ef6afa17f11e5f1503aab4
mduhovniy <maxim.duhovniy@gmail.com>
2016-12-20 09:24:02 +0200

    done: Student creation chain fixed next: other chains Expression -> Level -> Teacher , function parsing

--- src/main/java/info/duhovniy/mathclasses/AppConfig.java
+++ src/main/java/info/duhovniy/mathclasses/AppConfig.java
@@ -1,16 +1,30 @@
 package info.duhovniy.mathclasses;
 
+import com.mongodb.MongoClient;
 import info.duhovniy.mathclasses.commons.MathUtils;
 import info.duhovniy.mathclasses.commons.MathUtilsImpl;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.mongodb.core.MongoTemplate;
+import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
 
 
 @Configuration
+@EnableMongoRepositories
 public class AppConfig {
 
     @Bean
     public MathUtils mathUtils() {
         return new MathUtilsImpl();
+    }
+
+    @Bean
+    public MongoClient mongoClient() {
+        return new MongoClient();
+    }
+
+    @Bean
+    public MongoTemplate mongoTemplate() {
+        return new MongoTemplate(mongoClient(), "mathclasses");
     }
 }
--- src/main/java/info/duhovniy/mathclasses/dao/StudentRepository.java
+++ src/main/java/info/duhovniy/mathclasses/dao/StudentRepository.java
@@ -3,12 +3,10 @@ package info.duhovniy.mathclasses.dao;
 
 import info.duhovniy.mathclasses.dto.Student;
 import org.springframework.data.mongodb.repository.MongoRepository;
-import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
 import org.springframework.stereotype.Repository;
 
 
 @Repository
-@EnableMongoRepositories
 public interface StudentRepository extends MongoRepository<Student, String> {
 
     Student findStudentByName(String name);
